0974d51994b012ee6899b20909ed4041bed293b8
cpockrandt <noreply-github@cpockrandt.de>
2019-08-26 04:09:23 -0400

    pathes changed

--- test/performance/search/search_benchmark.cpp
+++ test/performance/search/search_benchmark.cpp
@@ -26,9 +26,11 @@ struct sequence_file_input_default_traits_aa10 : sequence_file_input_default_tra
      using sequence_legal_alphabet = aa10murphy;
 };
 
-constexpr uint64_t genome_len{500'000'000};
+constexpr uint64_t genome_len{1'000'000'000};
 constexpr uint64_t read_len{100};
 constexpr uint64_t read_count{1'000'000};
+
+std::filesystem::path root{"/home/pocki/epr_bench/"};
 
 // sequence_file_input<sequence_file_input_default_traits_aa10> fin{"/home/pocki/Downloads/murphy10.fa"};
 // std::vector<aa10murphy> genome;
@@ -40,125 +42,125 @@ constexpr uint64_t read_count{1'000'000};
 //
 // std::cout << genome.size() << " genome bases." << std::endl;
 
-// static void fm_index_build_data(benchmark::State& /*state*/) {
+static void fm_index_build_data(benchmark::State& /*state*/) {
+
+    std::vector<aa10murphy> genome(genome_len);
+
+    {
+        // build and store file
+        std::srand(std::time(nullptr));
+        for (uint64_t i = 0; i < genome.size(); ++i)
+            genome[i].assign_rank(std::rand() % 10);
+
+        std::filesystem::path p1{root / std::filesystem::path{"murphy10.fa"}};
+        sequence_file_output fout{p1};
+        fout.options.fasta_letters_per_line = 120;
+        fout.emplace_back(genome, "seq0");
+        std::cout << "Genome built." << std::endl;
+
+        // sample reads
+        std::filesystem::path p2{root / "reads.fa"};
+        sequence_file_output freads{p2};
+        freads.options.fasta_letters_per_line = 0;
+        for (uint64_t i = 0; i < read_count; ++i)
+        {
+            // max startPos is: genome_length - read_len
+            uint64_t const startPos{std::rand() % (genome_len - read_len + 1)};
+            std::vector<aa10murphy> read{genome | ranges::view::slice(startPos, startPos + read_len - 1)};
+            std::string id{"seq" + std::to_string(i)};
+            freads.push_back(std::tie(read, id));
+        }
+        std::cout << "Reads written." << std::endl;
+    }
+
+    {
+        fm_index wt(genome);
+
+        std::filesystem::path p3{root / "index.wt"};
+        std::ofstream os{p3, std::ios::binary};
+        cereal::BinaryOutputArchive oarchive{os};
+        oarchive(wt);
+
+        std::cout << "WT Index built." << std::endl;
+    }
+
+    {
+        fm_index<text_layout::single, sdsl_epr_index_type> epr(genome);
+
+        std::filesystem::path p4{root / "index.epr"};
+        std::ofstream os{p4, std::ios::binary};
+        cereal::BinaryOutputArchive oarchive{os};
+        oarchive(epr);
+
+        std::cout << "EPR Index built." << std::endl;
+    }
+
+    exit(0);
+}
+
+// static void fm_index_wt_benchmark(benchmark::State& state) {
+//     sequence_file_input<sequence_file_input_default_traits_aa10> fin{root / "reads.fa"};
+//     std::vector<std::vector<aa10murphy>> reads;
+//     reads.reserve(read_count);
 //
-//     std::vector<aa10murphy> genome(genome_len);
+//     for (auto & rec : fin)
+//         reads.emplace_back(get<field::SEQ>(rec));
 //
+//     std::cout << reads.size() << " reads read." << std::endl;
+//
+//     fm_index wt;
+//
+//     std::filesystem::path p3{root / "index.wt"};
+//     std::ifstream os{p3, std::ios::binary};
+//     cereal::BinaryInputArchive iarchive{os};
+//     iarchive(wt);
+//
+//     uint64_t count = 0;
+//     for (auto _ : state)
 //     {
-//         // build and store file
-//         std::srand(std::time(nullptr));
-//         for (uint64_t i = 0; i < genome.size(); ++i)
-//             genome[i].assign_rank(std::rand() % 10);
-//
-//         std::filesystem::path p1{"/home/pocki/Downloads/murphy10.fa"};
-//         sequence_file_output fout{p1};
-//         fout.options.fasta_letters_per_line = 120;
-//         fout.emplace_back(genome, "seq0");
-//         std::cout << "Genome built." << std::endl;
-//
-//         // sample reads
-//         std::filesystem::path p2{"/home/pocki/Downloads/reads.fa"};
-//         sequence_file_output freads{p2};
-//         freads.options.fasta_letters_per_line = 0;
-//         for (uint64_t i = 0; i < read_count; ++i)
+//         for (uint64_t i = 0; i < reads.size(); ++i)
 //         {
-//             // max startPos is: genome_length - read_len
-//             uint64_t const startPos{std::rand() % (genome_len - read_len + 1)};
-//             std::vector<aa10murphy> read{genome | ranges::view::slice(startPos, startPos + read_len - 1)};
-//             std::string id{"seq" + std::to_string(i)};
-//             freads.push_back(std::tie(read, id));
+//             auto it = wt.begin();
+//             it.extend_right(reads[i]);
+//             count += it.count();
 //         }
-//         std::cout << "Reads written." << std::endl;
 //     }
+//     std::cout << "Hits: " << count << std::endl;
+// }
 //
+// static void fm_index_epr_benchmark(benchmark::State& state) {
+//     sequence_file_input<sequence_file_input_default_traits_aa10> fin{root / "reads.fa"};
+//     std::vector<std::vector<aa10murphy>> reads;
+//     reads.reserve(read_count);
+//
+//     for (auto & rec : fin)
+//         reads.emplace_back(get<field::SEQ>(rec));
+//
+//     std::cout << reads.size() << " reads read." << std::endl;
+//
+//     fm_index<text_layout::single, sdsl_epr_index_type> epr;
+//
+//     std::filesystem::path p3{root / "index.epr"};
+//     std::ifstream os{p3, std::ios::binary};
+//     cereal::BinaryInputArchive iarchive{os};
+//     iarchive(epr);
+//
+//     uint64_t count = 0;
+//     for (auto _ : state)
 //     {
-//         fm_index wt(genome);
-//
-//         std::filesystem::path p3{"/home/pocki/Downloads/index.wt"};
-//         std::ofstream os{p3, std::ios::binary};
-//         cereal::BinaryOutputArchive oarchive{os};
-//         oarchive(wt);
-//
-//         std::cout << "WT Index built." << std::endl;
+//         for (uint64_t i = 0; i < reads.size(); ++i)
+//         {
+//             auto it = epr.begin();
+//             it.extend_right(reads[i]);
+//             count += it.count();
+//         }
 //     }
-//
-//     {
-//         fm_index<text_layout::single, sdsl_epr_index_type> epr(genome);
-//
-//         std::filesystem::path p4{"/home/pocki/Downloads/index.epr"};
-//         std::ofstream os{p4, std::ios::binary};
-//         cereal::BinaryOutputArchive oarchive{os};
-//         oarchive(epr);
-//
-//         std::cout << "EPR Index built." << std::endl;
-//     }
-//
-//     exit(0);
+//     std::cout << "Hits: " << count << std::endl;
 // }
 
-static void fm_index_wt_benchmark(benchmark::State& state) {
-    sequence_file_input<sequence_file_input_default_traits_aa10> fin{"/home/pocki/Downloads/reads.fa"};
-    std::vector<std::vector<aa10murphy>> reads;
-    reads.reserve(read_count);
-
-    for (auto & rec : fin)
-        reads.emplace_back(get<field::SEQ>(rec));
-
-    std::cout << reads.size() << " reads read." << std::endl;
-
-    fm_index wt;
-
-    std::filesystem::path p3{"/home/pocki/Downloads/index.wt"};
-    std::ifstream os{p3, std::ios::binary};
-    cereal::BinaryInputArchive iarchive{os};
-    iarchive(wt);
-
-    uint64_t count = 0;
-    for (auto _ : state)
-    {
-        for (uint64_t i = 0; i < reads.size(); ++i)
-        {
-            auto it = wt.begin();
-            it.extend_right(reads[i]);
-            count += it.count();
-        }
-    }
-    std::cout << "Hits: " << count << std::endl;
-}
-
-static void fm_index_epr_benchmark(benchmark::State& state) {
-    sequence_file_input<sequence_file_input_default_traits_aa10> fin{"/home/pocki/Downloads/reads.fa"};
-    std::vector<std::vector<aa10murphy>> reads;
-    reads.reserve(read_count);
-
-    for (auto & rec : fin)
-        reads.emplace_back(get<field::SEQ>(rec));
-
-    std::cout << reads.size() << " reads read." << std::endl;
-
-    fm_index<text_layout::single, sdsl_epr_index_type> epr;
-
-    std::filesystem::path p3{"/home/pocki/Downloads/index.epr"};
-    std::ifstream os{p3, std::ios::binary};
-    cereal::BinaryInputArchive iarchive{os};
-    iarchive(epr);
-
-    uint64_t count = 0;
-    for (auto _ : state)
-    {
-        for (uint64_t i = 0; i < reads.size(); ++i)
-        {
-            auto it = epr.begin();
-            it.extend_right(reads[i]);
-            count += it.count();
-        }
-    }
-    std::cout << "Hits: " << count << std::endl;
-}
-
 // Register the function as a benchmark
-// BENCHMARK(fm_index_build_data);
-BENCHMARK(fm_index_wt_benchmark)->Unit(benchmark::kMillisecond);
-BENCHMARK(fm_index_epr_benchmark)->Unit(benchmark::kMillisecond);
+BENCHMARK(fm_index_build_data);
+// BENCHMARK(fm_index_wt_benchmark)->Unit(benchmark::kMillisecond);
+// BENCHMARK(fm_index_epr_benchmark)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
